73118f93b4534b6a39e9d1cabd0dc0341e13ce7b
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2025-06-12 20:49:10 +0200

    feat: add django-cors-headers for CORS support in development

--- backend/giviz_backend/settings.py
+++ backend/giviz_backend/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = "django-insecure-d)7df@+2jq2wnu53c5(@^3-nw*nz7q8cxg!6@%m#r%4&3%p*i)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -49,6 +51,12 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+# CORS settings
+CORS_ALLOW_ALL_ORIGINS = True  # ONLY FOR DEVELOPMENT!
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+# ]
 
 ROOT_URLCONF = "giviz_backend.urls"
 
