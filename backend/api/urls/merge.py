c4d252ebaac380693156bea8c8bbf35d3ed0e5c1
Jorge Muñoz Rodríguez <jorgemr@protonmail.ch>
2025-07-12 10:24:04 +0200

    feat: add contributors_by_category view to group contributions by category

--- backend/api/urls/merge.py
+++ backend/api/urls/merge.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from api.views.merge import merge_contributions_view
+from api.views.merge import merge_contributions_view, contributors_by_category_view
 
 urlpatterns = [
     path("", merge_contributions_view, name="merge_contributions"),
+    path(
+        "contributors_by_category/",
+        contributors_by_category_view,
+        name="contributors_by_category",
+    ),
 ]
